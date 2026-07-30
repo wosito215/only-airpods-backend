6c98cb24d46f1bf43848b8d1e0f0bc7044c6e2f2
Juan Gutierrez <danonimo452@gmail.com>
2026-07-30 16:20:35 -0500

    Actualizar API_URL a Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Forzar los DNS públicos de Google
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Conectado: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
